2f605d6ac5f7eb142a829e1d9154c00286160978
meetshah2904 <meetshah6445@gmail.com>
2023-07-08 14:21:24 +0530

    bcrypt

--- app/src/Controller/SessionControllerdb.js
+++ app/src/Controller/SessionControllerdb.js
@@ -1,11 +1,15 @@
 const UserModel = require("../Model/UserModel")
 const jwt = require("jsonwebtoken")
 const { SEC_KEY } = process.env
+const bcrypt = require("bcrypt")
 
 module.exports.signup = async function (req, res) {
     let firstname = req.body.firstname
     let email = req.body.email
     let password = req.body.password
+    let encpass = bcrypt.hashSync(password,10)
+    // console.log(encpass);
+    req.body.password = encpass
     let user = new UserModel(req.body)
     let data = await user.save()
     res.json({ "msg": "SignUpDone", data: user, rcode: 200 })
@@ -15,7 +19,7 @@ module.exports.login = async function (req, res) {
     let email = req.body.email
     let password = req.body.password
     let user = await UserModel.findOne({ email: email })
-    if (user && user.password == password) {
+    if (user && bcrypt.compareSync(password,user.password)) {
         // token = parseInt(Math.random() * 1000)
         // user.token = token
         token = jwt.sign({ "authId":user._id,"authority":"user"},SEC_KEY,{expiresIn:"1d"})
@@ -24,4 +28,10 @@ module.exports.login = async function (req, res) {
     } else {
         res.json({ data: req.body, msg: "Invalid Credentials", rcode: -9 })
     }
+}
+
+module.exports.getAllusers = function(req,res){
+    UserModel.find().then(data=>{
+        res.json({data:data,msg:"User ret",rcode:200})
+    })
 }
--- app/src/routes/public.routes.js
+++ app/src/routes/public.routes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post("/signupdb", sessionControllerDb.signup)
 router.post("/login", sessionControllerDb.login)
+router.get("/getallusers", sessionControllerDb.getAllusers)
 
 module.exports = router
